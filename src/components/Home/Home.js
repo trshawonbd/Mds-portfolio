90295fceb7f3dbfc36be2e7feec49afc5f74ee3a
Your Name <you@example.com>
2022-04-10 11:09:57 +0300

    working on header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header/Header';
 import Profile from './Profile/Profile';
 import Footer from './Footer/Footer';
 import './Home.css'
 const Home = () => {
     return (
         <div className='home-container'>
+            <Header></Header>
             <Profile></Profile>
             <Footer></Footer>
             
